2c2aef0f38820c692932882cf870ab19a27a650b
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-03-17 12:36:36 +0800

    Inherit print_info

--- vehicle.py
+++ vehicle.py
@@ -5,16 +5,12 @@ class Vehicle():
         self.speed = speed # in kph
     
     def print_info(self):
-        print(f"This vehicle is so fast it reaches speeds of {self.speed}kph. It has a {self.color} finish, and the license plate is {self.license_plate}")
+        print(f"This {self.model if self.model else 'Vehicle'} is so fast it reaches speeds of {self.speed}kph. It has a {self.color} finish, and the license plate is {self.license_plate}")
 
 class Bus(Vehicle):
     def __init__(self, license_plate, color, speed, model: str):
         super().__init__(license_plate, color, speed)
         self.model = model
-        
-    def print_info(self):
-        print(f"This {self.model} is so fast it reaches speeds of {self.speed}kph. It has a {self.color} finish, and the license plate is {self.license_plate}")
-
 
 
 limbus = Bus("666-666", "dirty brown", 110.5, "Limbus")
